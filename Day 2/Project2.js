56c974b19c367254db84312999676081ffd75aaf
akshay-vaskar <64828725+akshay-vaskar@users.noreply.github.com>
2020-09-04 12:14:59 +0530

    Create Project2.js

---
+++ Day 2/Project2.js
@@ -0,0 +1,7 @@
+// Question 2: Program to convert minutes to seconds
+
+//Taking minutes from user
+let minutes = prompt("Enter minutes to convert to seconds");
+
+//Displaying the minutes converted to seconds
+console.log("Seconds = "+ Math.floor(minutes*60))
