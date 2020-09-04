f2691815d0e4e30cda292a66ee346608bc449464
akshay-vaskar <64828725+akshay-vaskar@users.noreply.github.com>
2020-09-04 12:15:44 +0530

    Delete Program2.js

--- Day 1/Program2.js
+++
@@ -1,7 +0,0 @@
-// Question 2: Program to convert minutes to seconds
-
-//Taking minutes from user
-let minutes = prompt("Enter minutes to convert to seconds");
-
-//Displaying the minutes converted to seconds
-console.log("Seconds = "+ Math.floor(minutes*60))
